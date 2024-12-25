5fcda6eff95d3fae64086bb4cad7aec8c9198204
mahamudul hasan seber <sebermh@gmail.com>
2024-12-26 00:45:49 +0600

    Added code

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -26,7 +26,7 @@ const Navbar = () => {
             <div className="navbar p-6">
                 <div className="navbar-start">
                     <div className="dropdown">
-                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <div tabIndex={0} role="button" className="btn btn-ghost text-white lg:hidden">
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
                                 className="h-5 w-5"

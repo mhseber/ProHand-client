59758b0c4e80c52377c2df0560790754a0b935a5
mahamudul hasan seber <sebermh@gmail.com>
2025-05-11 21:25:36 +0600

    Add SweetAlert

--- src/Pages/BookNow.jsx
+++ src/Pages/BookNow.jsx
@@ -1,186 +1,150 @@
 import { useState } from "react";
 
-
-
 const BookNow = () => {
-    const [showModal, setShowModal] = useState(true);
+  const [showModal, setShowModal] = useState(true);
 
-    if (!showModal) return null;
-    return (
-        <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50 z-50">
-            <div className=" rounded-lg p-6 md:p-8 w-full max-w-lg sm:max-w-3xl bg-orange-800 max-h-screen overflow-y-auto">
-                <div className="flex justify-between items-center mb-6">
-                    <h2 className="text-xl sm:text-2xl font-semibold text-gray-800 dark:text-gray-200">
-                        Book Service
-                    </h2>
-                    <button
-                        onClick={() => setShowModal(false)}
-                        className="text-gray-600 hover:text-gray-900"
-                    >
-                        <span className="font-bold text-2xl dark:text-gray-200">
-                            &times;
-                        </span>
-                    </button>
-                </div>
-
-                <form
-                // onSubmit={handleSubmit}
-                >
-                    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
-                        {/* Service ID */}
-                        <div>
-                            <label className="block text-gray-700 dark:text-gray-300">
-                                Service ID
-                            </label>
-                            <input
-                                type="text"
-                                name="serviceId"
-                                // value={formData.serviceId}
-                                readOnly
-                                className="w-full p-3 mt-2 border rounded-lg bg-white  outline-none"
-                            />
-                        </div>
-
-                        {/* Service Name */}
-                        <div>
-                            <label className="block text-gray-700 dark:text-gray-300">
-                                Service Name
-                            </label>
-                            <input
-                                type="text"
-                                name="serviceName"
-                                // value={formData.serviceName}
-                                readOnly
-                                className="w-full p-3 mt-2 border rounded-lg bg-white outline-none"
-                            />
-                        </div>
-
-                        {/* Service Image */}
-                        <div>
-                            <label className="block text-gray-700 dark:text-gray-300">
-                                Service Image
-                            </label>
-                            <input
-                                type="text"
-                                name="serviceImage"
-                                // value={formData.serviceImage}
-                                readOnly
-                                className="w-full p-3 mt-2 border rounded-lg bg-white  outline-none"
-                            />
-                        </div>
-
-                        {/* Provider Email */}
-                        <div>
-                            <label className="block text-gray-700 dark:text-gray-300">
-                                Provider Email
-                            </label>
-                            <input
-                                type="text"
-                                name="providerEmail"
-                                // value={formData.providerEmail}
-                                readOnly
-                                className="w-full p-3 mt-2 border rounded-lg bg-white  outline-none"
-                            />
-                        </div>
-
-                        {/* Provider Name */}
-                        <div>
-                            <label className="block text-gray-700 dark:text-gray-300">
-                                Provider Name
-                            </label>
-                            <input
-                                type="text"
-                                name="providerName"
-                                // value={formData.providerName}
-                                readOnly
-                                className="w-full p-3 mt-2 border rounded-lg bg-white  outline-none"
-                            />
-                        </div>
-
-                        {/* Current User Email */}
-                        <div>
-                            <label className="block text-gray-700 dark:text-gray-300">
-                                Current User Email
-                            </label>
-                            <input
-                                type="text"
-                                name="userEmail"
-                                // value={formData.userEmail}
-                                readOnly
-                                className="w-full p-3 mt-2 border rounded-lg bg-white  outline-none"
-                            />
-                        </div>
-
-                        {/* Current User Name */}
-                        <div>
-                            <label className="block text-gray-700 dark:text-gray-300">
-                                Current User Name
-                            </label>
-                            <input
-                                type="text"
-                                name="userName"
-                                // value={formData.userName}
-                                readOnly
-                                className="w-full p-3 mt-2 border rounded-lg bg-white  outline-none"
-                            />
-                        </div>
-
-                        {/* Service Taking Date */}
-                        <div>
-                            <label className="block text-gray-700 dark:text-gray-300">
-                                Service Taking Date
-                            </label>
-                            <input
-                                type="date"
-                                name="serviceDate"
-                                // value={formData.serviceDate}
-                                // onChange={handleChange}
-                                className="w-full p-3 mt-2 border bg-white rounded-lg outline-none"
-                            />
-                        </div>
-
-                        {/* Special Instructions */}
-                        <div className="col-span-1 sm:col-span-2">
-                            <label className="block text-gray-700 dark:text-gray-300">
-                                Special Instructions
-                            </label>
-                            <textarea
-                                name="specialInstructions"
-                                // value={formData.specialInstructions}
-                                // onChange={handleChange}
-                                className="w-full p-3 mt-2 border rounded-lg bg-white  outline-none"
-                                placeholder="Anything like address, area, customized service plan..."
-                            />
-                        </div>
-
-                        {/* Price */}
-                        <div>
-                            <label className="block text-gray-700 dark:text-gray-300">
-                                Price
-                            </label>
-                            <input
-                                type="text"
-                                name="price"
-                                // value={formData.price}
-                                readOnly
-                                className="w-full p-3 mt-2 border rounded-lg bg-white  outline-none"
-                            />
-                        </div>
-                    </div>
-
-                    {/* Purchase Button */}
-                    <div className="mt-6">
-                        <button
-                            type="submit"
-                            className="w-full px-6 py-3 bg-gradient-to-r from-gray-500 to-gray-800 text-white rounded-lg font-semibold hover:bg-green-700 dark:text-gray-300"
-                        >
-                            Purchase
-                        </button>
-                    </div>
-                </form>
-            </div>
+  if (!showModal) return null;
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-800 bg-opacity-50">
+      <div className="w-full max-w-lg max-h-screen p-6 overflow-y-auto bg-orange-700 rounded-lg md:p-8 sm:max-w-3xl">
+        <div className="flex items-center justify-between mb-6">
+          <h2 className="text-xl font-semibold text-white sm:text-2xl">
+            Book Service
+          </h2>
+          <button
+            onClick={() => setShowModal(false)}
+            className="text-gray-600 hover:text-gray-900"
+          >
+            <span className="text-2xl font-bold dark:text-gray-200">
+              &times;
+            </span>
+          </button>
         </div>
-    );
+
+        <form>
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+            {/* Service ID */}
+            <div>
+              <label className="block text-white">Service ID</label>
+              <input
+                type="text"
+                name="serviceId"
+                readOnly
+                className="w-full p-3 mt-2 bg-white border rounded-lg outline-none"
+              />
+            </div>
+
+            {/* Service Name */}
+            <div>
+              <label className="block text-white">Service Name</label>
+              <input
+                type="text"
+                name="serviceName"
+                readOnly
+                className="w-full p-3 mt-2 bg-white border rounded-lg outline-none"
+              />
+            </div>
+
+            {/* Service Image */}
+            <div>
+              <label className="block text-white">Service Image</label>
+              <input
+                type="text"
+                name="serviceImage"
+                readOnly
+                className="w-full p-3 mt-2 bg-white border rounded-lg outline-none"
+              />
+            </div>
+
+            {/* Provider Email */}
+            <div>
+              <label className="block text-white">Provider Email</label>
+              <input
+                type="text"
+                name="providerEmail"
+                readOnly
+                className="w-full p-3 mt-2 bg-white border rounded-lg outline-none"
+              />
+            </div>
+
+            {/* Provider Name */}
+            <div>
+              <label className="block text-white">Provider Name</label>
+              <input
+                type="text"
+                name="providerName"
+                readOnly
+                className="w-full p-3 mt-2 bg-white border rounded-lg outline-none"
+              />
+            </div>
+
+            {/* Current User Email */}
+            <div>
+              <label className="block text-white">Current User Email</label>
+              <input
+                type="text"
+                name="userEmail"
+                readOnly
+                className="w-full p-3 mt-2 bg-white border rounded-lg outline-none"
+              />
+            </div>
+
+            {/* Current User Name */}
+            <div>
+              <label className="block text-white">Current User Name</label>
+              <input
+                type="text"
+                name="userName"
+                readOnly
+                className="w-full p-3 mt-2 bg-white border rounded-lg outline-none"
+              />
+            </div>
+
+            {/* Service Taking Date */}
+            <div>
+              <label className="block text-white">Service Taking Date</label>
+              <input
+                type="date"
+                name="serviceDate"
+                className="w-full p-3 mt-2 bg-white border rounded-lg outline-none"
+              />
+            </div>
+
+            {/* Special Instructions */}
+            <div className="col-span-1 sm:col-span-2">
+              <label className="block text-white">Special Instructions</label>
+              <textarea
+                name="specialInstructions"
+                className="w-full p-3 mt-2 bg-white border rounded-lg outline-none"
+                placeholder="Anything like address, area, customized service plan..."
+              />
+            </div>
+
+            {/* Price */}
+            <div>
+              <label className="block text-white">Price</label>
+              <input
+                type="text"
+                name="price"
+                readOnly
+                className="w-full p-3 mt-2 bg-white border rounded-lg outline-none"
+              />
+            </div>
+          </div>
+
+          {/* Purchase Button */}
+          <div className="mt-6">
+            <button
+              type="submit"
+              className="w-full px-6 py-3 font-semibold text-white rounded-lg bg-gradient-to-r from-gray-500 to-gray-800 hover:bg-green-700 dark:text-gray-300"
+            >
+              Purchase
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default BookNow;
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,99 +1,139 @@
 import Lottie from "lottie-react";
-import { Link } from "react-router-dom";
-import loginLottie from "../assets/Lottie/lottie/login.json"
+import { Link, useNavigate } from "react-router-dom";
+import loginLottie from "../assets/Lottie/lottie/login.json";
 import { FcGoogle } from "react-icons/fc";
 import { useContext } from "react";
 import AuthContext from "../Context/AuthContext/AuthContext";
-
+import Swal from "sweetalert2";
 
 const Login = () => {
-    const { signInUser, signInWithGoogle } = useContext(AuthContext)
+  const { signInUser, signInWithGoogle } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const handleSignIn = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email, password);
 
-    const handleSignIn = e => {
-        e.preventDefault();
-        const form = e.target;
-        const email = form.email.value;
-        const password = form.password.value;
-        console.log(email, password);
+    signInUser(email, password)
+      .then((result) => {
+        console.log("sign InUser", result.user);
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: "Login Successful!",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+        setTimeout(() => {
+          navigate("/");
+        }, 1600);
+      })
+      .catch((error) => {
+        console.log(error);
+        Swal.fire({
+          icon: "error",
+          title: "Login Failed",
+          text: error.message,
+        });
+      });
+  };
 
-        signInUser(email, password)
-            .then(result => {
-                console.log('sign InUser', result.user);
-            })
-            .catch(error => {
-                console.log(error);
-            })
-    };
+  const handleGoogleSignIn = () => {
+    signInWithGoogle()
+      .then((result) => {
+        console.log("Google sign-in success:", result.user);
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: "Google Login Successful!",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+        setTimeout(() => {
+          navigate("/");
+        }, 1600);
+      })
+      .catch((error) => {
+        console.error("Google sign-in error:", error);
+        Swal.fire({
+          icon: "error",
+          title: "Google Sign-In Failed",
+          text: error.message,
+        });
+      });
+  };
+  return (
+    <div className="items-center justify-center min-h-screen pt-3 pl-4 text-gray-900 bg-gray-100 lg:flex dark:bg-gray-800 dark:text-white">
+      <div className="w-full max-w-md p-6 bg-white rounded-lg shadow-xl shadow-black">
+        <h1 className="mb-6 text-2xl font-bold text-center text-black">
+          User SignIn
+        </h1>
+        <form onSubmit={handleSignIn}>
+          {/* Email Field */}
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block mb-2 font-medium text-gray-700"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Enter your email"
+              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+              required
+            />
+          </div>
 
-    const handleGoogleSignIn = () => {
-        signInWithGoogle()
-            .then((result) => {
-                console.log("Google sign-in success:", result.user);
-            })
-            .catch((error) => {
-                console.error("Google sign-in error:", error);
-            });
-    };
-    return (
+          {/* Password Field */}
+          <div className="mb-4">
+            <label
+              htmlFor="password"
+              className="block mb-2 font-medium text-gray-700"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Enter your password"
+              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+              required
+            />
+          </div>
+          {/* Login Button */}
+          <button
+            type="submit"
+            className="w-full py-2 font-semibold text-white rounded-md bg-gradient-to-r from-gray-500 to-gray-800"
+          >
+            Login
+          </button>
+        </form>
+        <button
+          onClick={handleGoogleSignIn}
+          className="w-full mt-4 text-xl font-bold text-black btn "
+        >
+          <FcGoogle />
+          Sign In With Google
+        </button>
 
-        <div className="lg:flex pl-4 pt-3 justify-center items-center min-h-screen  bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-white">
-            <div className="bg-white shadow-xl shadow-black rounded-lg p-6 w-full max-w-md">
-                <h1 className="text-2xl font-bold text-center mb-6 text-black">User SignIn</h1>
-                <form onSubmit={handleSignIn}>
-                    {/* Email Field */}
-                    <div className="mb-4">
-                        <label htmlFor="email" className="block text-gray-700 font-medium mb-2">
-                            Email
-                        </label>
-                        <input
-                            type="email"
-                            id="email"
-                            name="email"
-                            placeholder="Enter your email"
-                            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
-                            required
-                        />
-                    </div>
-
-                    {/* Password Field */}
-                    <div className="mb-4">
-                        <label htmlFor="password" className="block text-gray-700 font-medium mb-2">
-                            Password
-                        </label>
-                        <input
-                            type="password"
-                            id="password"
-                            name="password"
-                            placeholder="Enter your password"
-                            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
-                            required
-                        />
-                    </div>
-                    {/* Login Button */}
-                    <button
-                        type="submit"
-                        className="w-full  text-white py-2 rounded-md bg-gradient-to-r from-gray-500 to-gray-800  font-semibold"
-                    >
-                        Login
-                    </button>
-                </form>
-                <button
-                    onClick={handleGoogleSignIn}
-                    className="w-full mt-4 btn   text-black font-bold text-xl ">
-                    <FcGoogle />
-                    Sign In With Google</button>
-
-                <p className="text-center  font-bold pt-5  ">
-                    You Have no Account ?
-                    <Link to="/auth/register"><span className="text-black pl-5">Register</span></Link>
-                </p>
-
-            </div>
-            <div className=" w-80 pl-8 pt-4">
-                <Lottie animationData={loginLottie}></Lottie>
-            </div>
-        </div>
-    );
+        <p className="pt-5 font-bold text-center dark:text-black ">
+          You Have no Account ?
+          <Link to="/auth/register">
+            <span className="pl-5 text-black">Register</span>
+          </Link>
+        </p>
+      </div>
+      <div className="pt-4 pl-8 w-80">
+        <Lottie animationData={loginLottie}></Lottie>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
